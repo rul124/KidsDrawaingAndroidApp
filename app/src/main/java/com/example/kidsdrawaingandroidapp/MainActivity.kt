39d1fa5419f60d03ec85988f34b853c7e5d3d167
Rosie Liu <rosie.liurx@gmail.com>
2022-03-04 17:19:17 -0500

    add brush size chooser

--- app/src/main/java/com/example/kidsdrawaingandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/kidsdrawaingandroidapp/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.kidsdrawaingandroidapp
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     private var drawingView: DrawingView? = null
@@ -13,5 +15,35 @@ class MainActivity : AppCompatActivity() {
         // set the brush size
         drawingView = findViewById(R.id.drawing_view)
         drawingView?.setSizeForBrush(20.toFloat())
+        
+        
+    }
+    
+    private fun showBrushSizeChooserDialog() {
+        // a pop-up
+        val brushDialog = Dialog(this)
+        // the dialog should look like this
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.setTitle("Brush size: ")
+
+        val smallBtn = brushDialog.findViewById<Button>(R.id.ib_small_brush)
+        smallBtn.setOnClickListener {
+            drawingView?.setSizeForBrush(10.toFloat())
+            brushDialog.dismiss()
+        }
+
+        val mediumBtn = brushDialog.findViewById<Button>(R.id.ib_medium_brush)
+        mediumBtn.setOnClickListener {
+            drawingView?.setSizeForBrush(20.toFloat())
+            brushDialog.dismiss()
+        }
+
+        val largeBtn = brushDialog.findViewById<Button>(R.id.ib_large_brush)
+        largeBtn.setOnClickListener {
+            drawingView?.setSizeForBrush(30.toFloat())
+            brushDialog.dismiss()
+        }
+
+        brushDialog.show()
     }
 }
