16d9420cea90c3150f3252032c96623530d1acff
Rosie Liu <rosie.liurx@gmail.com>
2022-03-04 18:42:38 -0500

    create color selector

--- app/src/main/java/com/example/kidsdrawaingandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/kidsdrawaingandroidapp/MainActivity.kt
@@ -5,13 +5,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
 
 class MainActivity : AppCompatActivity() {
     private var drawingView: DrawingView? = null
+    private var mImageButtonCurrentPaint : ImageButton? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // set brush color
+        val linearLayoutPaintColors : LinearLayout = findViewById(R.id.ll_paint_colors)
+        mImageButtonCurrentPaint = linearLayoutPaintColors[1] as ImageButton
+        // Set the image drawable
+        mImageButtonCurrentPaint!!.setImageDrawable(
+            ContextCompat.getDrawable(this, R.drawable.pallet_pressed)
+        )
 
         // set the brush size
         drawingView = findViewById(R.id.drawing_view)
@@ -21,6 +33,8 @@ class MainActivity : AppCompatActivity() {
         ibBrush.setOnClickListener{
             showBrushSizeChooserDialog()
         }
+
+
     }
     
     private fun showBrushSizeChooserDialog() {
